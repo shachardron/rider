35c1382c16449af61c287c7ca966d44eca5216b7
shachar.doron@gmail.com <Doron1971!!!>
2022-11-18 17:08:46 +0200

    test

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: '3mft3gntlv90plv6mhoqajjqpj', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: 'eu-central-1_YTWlTaeJY', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: ' eu-central-1_YTWlTaeJY', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3mft3gntlv90plv6mhoqajjqpj', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'eu-central-1' // e.g. us-east-2
     },
     api: {
